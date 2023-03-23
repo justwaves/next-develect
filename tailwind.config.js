f1d849435f1d589e76a928ede04e98d8174a1f57
euijin <justwavz@gmail.com>
2023-03-23 22:44:23 +0900

    [#3] 🔧 tailwind css screens theme 설정

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,11 @@ module.exports = {
   content: [],
   theme: {
     extend: {
+      screens: {
+        sm: '0px', // Mobile
+        md: '600px', // Tablet
+        lg: '1200px', // Desktop
+      },
       fontFamily: {
         sans: ['var(--font-pretendard)', ...fontFamily.sans],
       },
