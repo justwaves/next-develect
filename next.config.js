d0985f300c13c07d21a189cd7c2174a399ec8a83
euijin <justwavz@gmail.com>
2023-03-23 23:11:53 +0900

    [#3] 🔧 @svgr/webpack 설정

--- next.config.js
+++ next.config.js
@@ -4,6 +4,10 @@ const withTwin = require('./withTwin.js')
 const nextConfig = withTwin({
   reactStrictMode: true,
   webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    })
     return config
   },
 })
