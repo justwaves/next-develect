b4a66dadfbdc5aee6e8b97573c2f062e5bc9410e
euijin <justwavz@gmail.com>
2023-03-23 22:51:46 +0900

    [#3] ✨ BasicLayout 생성 및 페이지 getLayout 적용

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,22 @@
 import { pretendard } from '@/styles/font'
 
 import '@/styles/globals.css'
+import type { NextPage } from 'next'
 import type { AppProps } from 'next/app'
+import type { ReactElement, ReactNode } from 'react'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return (
+export type NextPageWithLayout<P = object, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode
+}
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout
+}
+
+export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+  const getLayout = Component.getLayout ?? ((page) => page)
+
+  return getLayout(
     <div className={`${pretendard.className} font-sans`}>
       <Component {...pageProps} />
     </div>
