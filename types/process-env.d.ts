2c52f098e1cd094b8afdba5508049bc41da95b7b
euijin <justwavz@gmail.com>
2023-04-07 14:46:59 +0900

    [#4] 🔐 DB 환경 변수 추가

---
+++ types/process-env.d.ts
@@ -0,0 +1,11 @@
+declare namespace NodeJS {
+  interface ProcessEnv {
+    NODE_ENV: 'development' | 'production' | 'test'
+    DB_USERNAME: string
+    DB_PASSWORD: string
+    DB_HOST: string
+    DB_PORT: string
+    DB_NAME: string
+    DATABASE_URL: string
+  }
+}
