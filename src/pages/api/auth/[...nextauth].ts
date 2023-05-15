60dc449582a54eee1612ca310fdb7c5580498fd9
euijin <justwavz@gmail.com>
2023-05-15 20:42:21 +0900

    [#15] 🔧 next-auth session user 설정

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,10 +1,10 @@
 import { PrismaAdapter } from '@next-auth/prisma-adapter'
-import NextAuth from 'next-auth'
+import NextAuth, { AuthOptions } from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
 
 import { prisma } from '@/lib/prisma/client'
 
-export const authOptions = {
+export const authOptions: AuthOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
@@ -12,6 +12,24 @@ export const authOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
+  callbacks: {
+    async session({ session, user }) {
+      const userInfo = {
+        ...session.user,
+        id: user.id,
+        nickname: user.nickname,
+      }
+
+      return {
+        ...session,
+        user: userInfo,
+      }
+    },
+    jwt: async ({ token }) => token,
+  },
+  pages: {
+    newUser: '/auth/new-user',
+  },
 }
 
 export default NextAuth(authOptions)
